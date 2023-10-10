711d6727f22a3ba81751758e10935ff4a104344b
RevEmmanuel <adeolaae1@gmail.com>
2023-10-10 10:39:16 +0100

    movie and qrcode service started

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QrcodeService } from './qrcode/qrcode.service';
+import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, QrcodeService],
+  providers: [AppService, QrcodeService, MoviesService],
 })
 export class AppModule {}
--- src/qrcode/qrcode.service.ts
+++ src/qrcode/qrcode.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import * as qrcode from 'qrcode';
 
 @Injectable()
-export class QrcodeService {}
+export class QrcodeService {
+  async generateQRCode(text: string): Promise<string> {
+    // Generate a QR code for the given text
+    return qrcode.toDataURL(text);
+  }
+}
